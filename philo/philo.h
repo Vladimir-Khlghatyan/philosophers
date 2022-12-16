d0f3781c953598f795634fbe4946c80f574feefe
Vladimir <94677889+Vladimir-Khlghatyan@users.noreply.github.com>
2022-12-17 00:21:30 +0400

    color code upd

--- philo/philo.h
+++ philo/philo.h
@@ -19,12 +19,14 @@
 # include <stdio.h>
 # include <sys/time.h>
 
-# define RED        "\33[1;31m"
-# define GREEN      "\33[1;32m"
-# define PINK	    "\33[1;35m"
-# define YELLOW		"\33[1;33m"
-# define BLUE     	"\33[1;36m"
-# define CYAN       "\33[1;36m"
+# define RED	"\33[1;31m"
+# define GREEN	"\33[1;32m"
+# define YELLOW	"\33[1;33m"
+# define BLUE	"\33[1;34m"
+# define PINK	"\33[1;35m"
+# define CYAN	"\33[1;36m"
+# define WHITE	"\33[1;37m"
+# define RESET	"\33[0;m"
 
 typedef struct s_philo	t_philo;
 
